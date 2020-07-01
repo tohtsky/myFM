ac55c32e6d4a300f5a300d42752dc45eeac143ad
Tomoki Ohtsuki <tomoki.ohtsuki129@gmail.com>
2020-07-01 12:55:41 +0000

    making benchmard data loader as a part of the module

---
+++ myfm/utils/benchmark_data/movielens10M_data.py
@@ -0,0 +1,22 @@
+import os
+from io import BytesIO
+from movielens100k_data import MovieLens100kDataManager
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def _read_interaction(byte_stream):
+    with BytesIO(byte_stream) as ifs:
+        data = pd.read_csv(ifs, sep='\t', header=None, names=['user_id', 'movie_id', 'rating', 'timestamp'])
+        data['timestamp'] = pd.to_datetime(data['timestamp'], unit='s')
+        return data
+
+class MovieLens10MDataManager(MovieLens100kDataManager):
+    DOWNLOAD_URL = 'http://files.grouplens.org/datasets/movielens/ml-10m.zip'
+    DEFAULT_PATH = os.path.expanduser('~/.ml-10m.zip')
+    
+    def load_rating(self):
+        with BytesIO(self.zf.read('ml-10M100K/ratings.dat')) as ifs:
+            import pandas as pd
+            df = pd.read_csv(ifs, sep='\:\:', header=None, names=['user_id', 'movie_id', 'rating', 'timestamp'], engine='python')
+            df['timestamp'] = pd.to_datetime(df.timestamp, unit='s')
+            return df
