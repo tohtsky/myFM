755692ab0f5dab3324875594aa42278672c2c242
Tomoki Ohtsuki <tomoki.ohtsuki129@gmail.com>
2020-11-14 13:45:29 +0000

    More sophisticated data loader

--- myfm/utils/benchmark_data/movielens100k_data.py
+++ myfm/utils/benchmark_data/movielens100k_data.py
@@ -1,69 +1,82 @@
-from zipfile import ZipFile
 from io import BytesIO
 import os
-import urllib.request
+from typing import Optional, Tuple
 
 import pandas as pd
-from scipy import sparse as sps
-from sklearn.model_selection import train_test_split
+from .loader_base import MovieLensBase
 
 
-class MovieLens100kDataManager:
-    DOWNLOAD_URL = 'http://files.grouplens.org/datasets/movielens/ml-100k.zip'
-    DEFAULT_PATH = os.path.expanduser('~/.ml-100k.zip')
-    
-    @classmethod
-    def _read_interaction(cls, byte_stream):
+class MovieLens100kDataManager(MovieLensBase):
+    @property
+    def DOWNLOAD_URL(self) -> str:
+        "http://files.grouplens.org/datasets/movielens/ml-100k.zip"
+
+    @property
+    def DEFAULT_PATH(self) -> str:
+        return os.path.expanduser("~/.ml-100k.zip")
+
+    def _read_interaction(self, byte_stream: bytes) -> pd.DataFrame:
         with BytesIO(byte_stream) as ifs:
-            data = pd.read_csv(ifs, sep='\t', header=None, names=['user_id', 'movie_id', 'rating', 'timestamp'])
-            data['timestamp'] = pd.to_datetime(data['timestamp'], unit='s')
+            data = pd.read_csv(
+                ifs,
+                sep="\t",
+                header=None,
+                names=["user_id", "movie_id", "rating", "timestamp"],
+            )
+            data["timestamp"] = pd.to_datetime(data["timestamp"], unit="s")
             return data
 
-    def __init__(self, zippath=None):
-        if zippath is None:
-            zippath = self.DEFAULT_PATH
-            if not os.path.exists(zippath):
-                download = input('Could not find {}.\nCan I download and save it there?[y/N]'.format(zippath))
-                if download.lower() == 'y':
-                    print('start download...')
-                    urllib.request.urlretrieve(self.DOWNLOAD_URL, zippath)
-                    print('complete')
-                else:
-                    zippath = None
-                    
-        if zippath is not None:
-            self.zf = ZipFile(zippath)
-        else:
-            self.zf = None
-            
-    def load_rating(self, random_state=114514, fold=None):
-        if fold is None:
-            df_all = self._read_interaction(self.zf.read('ml-100k/u.data'))
-            df_train, df_test = train_test_split(df_all, random_state=random_state)
-        else:
-            assert fold >=1 and fold <=5
-            train_path = 'ml-100k/u{}.base'.format(fold)
-            test_path = 'ml-100k/u{}.test'.format(fold)
-            df_train = self._read_interaction(self.zf.read(train_path))
-            df_test = self._read_interaction(self.zf.read(test_path))
+    def load_rating_all(self) -> pd.DataFrame:
+        return self._read_interaction(self.zf.read("ml-100k/u.data"))
+
+    def load_rating_predifined_split(
+        self,
+        fold: int,
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """Read the pre-defined train/test split.
+        Fold index ranges from 1 to 5.
+
+        Parameters
+        ----------
+        fold : int
+            specifies the fold index.
+
+        Returns
+        -------
+        Tuple[pd.DataFrame, pd.DataFrame]
+            train and test dataframes.
+
+        """
+        assert fold >= 1 and fold <= 5
+        train_path = "ml-100k/u{}.base".format(fold)
+        test_path = "ml-100k/u{}.test".format(fold)
+        df_train = self._read_interaction(self.zf.read(train_path))
+        df_test = self._read_interaction(self.zf.read(test_path))
+
         return df_train, df_test
-    
-    def load_userinfo(self):
-        user_info_bytes = self.zf.read('ml-100k/u.user')
+
+    def load_user_info(self) -> pd.DataFrame:
+        user_info_bytes = self.zf.read("ml-100k/u.user")
         with BytesIO(user_info_bytes) as ifs:
-            return pd.read_csv(ifs, sep='|', header=None, names=['user_id', 'age', 'gender', 'occupation', 'zipcode'])
-    
-    def load_movieinfo(self):
-        MOVIE_COLUMNS = ['movie_id', 'title', 'release_date', 'unk', 'url']
-        with BytesIO(self.zf.read('ml-100k/u.genre')) as ifs:
-            genres = pd.read_csv(ifs, sep='|', header=None)[0]
-        with BytesIO(self.zf.read('ml-100k/u.item')) as ifs:
+            return pd.read_csv(
+                ifs,
+                sep="|",
+                header=None,
+                names=["user_id", "age", "gender", "occupation", "zipcode"],
+            )
+
+    def load_movie_info(self) -> pd.DataFrame:
+        MOVIE_COLUMNS = ["movie_id", "title", "release_date", "unk", "url"]
+
+        with BytesIO(self.zf.read("ml-100k/u.genre")) as ifs:
+            genres = pd.read_csv(ifs, sep="|", header=None)[0]
+        with BytesIO(self.zf.read("ml-100k/u.item")) as ifs:
             df_mov = pd.read_csv(
-                ifs, sep='|', encoding='latin-1',
+                ifs,
+                sep="|",
+                encoding="latin-1",
                 header=None,
             )
-            df_mov.columns = (
-                MOVIE_COLUMNS + list(genres) 
-            )
-        df_mov['release_date'] = pd.to_datetime(df_mov.release_date)
+            df_mov.columns = MOVIE_COLUMNS + list(genres)
+        df_mov["release_date"] = pd.to_datetime(df_mov.release_date)
         return df_mov, list(genres)
--- myfm/utils/benchmark_data/movielens1M_data.py
+++ myfm/utils/benchmark_data/movielens1M_data.py
@@ -1,16 +1,43 @@
+from typing import Tuple
 import os
 from io import BytesIO
 from .movielens100k_data import MovieLens100kDataManager
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
 class MovieLens1MDataManager(MovieLens100kDataManager):
-    DOWNLOAD_URL = 'http://files.grouplens.org/datasets/movielens/ml-1m.zip'
-    DEFAULT_PATH = os.path.expanduser('~/.ml-1m.zip')
-    
-    def load_rating(self, random_state=114514, test_size=0.1):
-        with BytesIO(self.zf.read('ml-1m/ratings.dat')) as ifs:
+    DOWNLOAD_URL = "http://files.grouplens.org/datasets/movielens/ml-1m.zip"
+    DEFAULT_PATH = os.path.expanduser("~/.ml-1m.zip")
+
+    def _read_interaction(self, byte_stream: bytes) -> pd.DataFrame:
+        with BytesIO(byte_stream) as ifs:
+            data = pd.read_csv(
+                ifs,
+                sep="\t",
+                header=None,
+                names=["user_id", "movie_id", "rating", "timestamp"],
+            )
+            data["timestamp"] = pd.to_datetime(data["timestamp"], unit="s")
+            return data
+
+    def load_rating_all(self) -> pd.DataFrame:
+        """Read all (1M) interactions.
+
+        Returns
+        -------
+        pd.DataFrame
+            [description]
+        """
+        with BytesIO(self.zf.read("ml-1m/ratings.dat")) as ifs:
             import pandas as pd
-            df = pd.read_csv(ifs, sep='\:\:', header=None, names=['user_id', 'movie_id', 'rating', 'timestamp'], engine='python')
-            df['timestamp'] = pd.to_datetime(df.timestamp, unit='s')
-            return train_test_split(df, random_state=random_state, test_size=test_size)
+
+            df = pd.read_csv(
+                ifs,
+                sep="\:\:",
+                header=None,
+                names=["user_id", "movie_id", "rating", "timestamp"],
+                engine="python",
+            )
+            df["timestamp"] = pd.to_datetime(df.timestamp, unit="s")
+            return df
