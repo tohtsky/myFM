59030c0968478e019473dcce327ec0870512d8bc
Tomoki <tomoki.ohtsuki129@gmail.com>
2022-01-16 22:25:27 +0900

    add first test

---
+++ tests/conftest.py
@@ -0,0 +1,46 @@
+from typing import Tuple, List
+import numpy as np
+import scipy.sparse as sps
+
+import pytest
+
+from .util import FMWeights, prediction
+
+N_FEATURES = 3
+N_LATENT = 4
+
+
+@pytest.fixture
+def stub_weight() -> FMWeights:
+    weights = FMWeights(
+        -3.0,
+        np.asfarray([1.0, 2.0, -1.0]),
+        np.asfarray(
+            [[1.0, -1.0, 0], [0.0, 1.0, 1.0], [1.0, 1.0, 1.0], [-1.0, 0, -1.0]]
+        ),
+    )
+    return weights
+
+
+def create_data(
+    n_train: int, stub_weight: FMWeights
+) -> Tuple[sps.csr_matrix, np.ndarray]:
+    rns = np.random.RandomState(0)
+    n = np.ones(N_FEATURES)
+    rows: List[int] = []
+    cols: List[int] = []
+    data: List[float] = []
+    for row in range(n_train):
+        indices = np.where(rns.random(N_FEATURES) > 0.5)[0]
+        for ind in indices:
+            rows.append(row)
+            cols.append(ind)
+            data.append(float(rns.choice([-2, -1, 1, 2])))
+    X = sps.csr_matrix((data, (rows, cols)))
+    p = prediction(X, weight=stub_weight)
+    return X, p
+
+
+@pytest.fixture
+def middle_data(stub_weight: FMWeights) -> Tuple[sps.csr_matrix, np.ndarray]:
+    return create_data(1000, stub_weight)
--- tests/regression/test_fit.py
+++ tests/regression/test_fit.py
@@ -1,2 +1,27 @@
+from typing import Tuple
 from scipy import sparse as sps
 import numpy as np
+from ..util import FMWeights
+
+from myfm import MyFMGibbsRegressor
+
+import pytest
+
+
+@pytest.mark.parametrize("alpha_inv", [0.1, 10.0, 1, 0.3, 3])
+def test_middle_reg(
+    alpha_inv: float,
+    middle_data: Tuple[sps.csr_matrix, np.ndarray],
+    stub_weight: FMWeights,
+) -> None:
+    rns = np.random.RandomState(0)
+    X, score = middle_data
+    y = score + alpha_inv * rns.normal(0, 1, size=score.shape)
+
+    fm = MyFMGibbsRegressor(4).fit(
+        X, y, X_test=X, y_test=y, n_iter=50  # , n_kept_samples=50
+    )
+    hp_trance = fm.get_hyper_trace()
+    last_alphs = hp_trance["alpha"].iloc[-20:].values
+    assert np.all(last_alphs > ((1 / alpha_inv ** 2) / 2))
+    assert np.all(last_alphs < ((1 / alpha_inv ** 2) * 2))
